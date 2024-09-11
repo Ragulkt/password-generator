83b23635f3e188534afe19940e3cf5090875125f
Ragulkt <162488047+Ragulkt@users.noreply.github.com>
2024-09-11 14:08:30 +0530

    Create password.py

---
+++ password.py
@@ -0,0 +1,60 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "5dfe4d27-4667-4b3e-af12-063439064356",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "password is =  s2Xjq@[C\n"
+     ]
+    }
+   ],
+   "source": [
+    "import random\n",
+    "\n",
+    "lower   = \"qwertyuiopasdfghjklzxcvbnm\"\n",
+    "upper   = \"QWERTYUIOPASDFGHJKLZXCVBNM\"\n",
+    "number  = \"1234567890\"\n",
+    "symbols = \"!@#$%^&*()_}{[]\"\n",
+    "all     =  lower + upper + number + symbols\n",
+    "length  = 8\n",
+    "password=\"\".join(random.sample(all,length))\n",
+    "print(\"password is = \",password)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "10b79f08-ee18-4255-a4f6-96ac42ba5268",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
